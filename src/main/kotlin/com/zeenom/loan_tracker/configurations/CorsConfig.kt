66b63462c7f8041d804e5d59eb9ba673987b6d04
zeeshan <zeeshan.tufail@aboitiz.com>
2025-01-30 02:28:04 +0800

    added CORS configuration

---
+++ src/main/kotlin/com/zeenom/loan_tracker/configurations/CorsConfig.kt
@@ -0,0 +1,24 @@
+package com.zeenom.loan_tracker.configurations
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+@Profile("dev")
+class CorsConfig {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*") // Allow localhost with port 3000
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
