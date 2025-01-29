e40f6ee9ee901a8af9318c7a947e5a9004c3049a
zeeshan <zeeshan.tufail@aboitiz.com>
2025-01-29 19:17:54 +0800

    trying to add swagger

--- src/main/kotlin/com/zeenom/loan_tracker/controllers/FriendsController.kt
+++ src/main/kotlin/com/zeenom/loan_tracker/controllers/FriendsController.kt
@@ -7,11 +7,13 @@ import com.zeenom.loan_tracker.services.toResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 class FriendsController(val queryFriendsService: QueryFriendsService) {
 
 
