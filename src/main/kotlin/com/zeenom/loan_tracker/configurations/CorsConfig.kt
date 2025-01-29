725351b98910e1f5d026e0f196ad69b1d9b2651e
zeeshan <zeeshan.tufail@aboitiz.com>
2025-01-30 03:01:54 +0800

    fixing cors for reactive web

--- src/main/kotlin/com/zeenom/loan_tracker/configurations/CorsConfig.kt
+++ src/main/kotlin/com/zeenom/loan_tracker/configurations/CorsConfig.kt
@@ -1,24 +1,45 @@
-package com.zeenom.loan_tracker.configurations
-
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.web.cors.reactive.CorsUtils
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
 
 @Configuration
 @Profile("dev")
 class CorsConfig {
+
+    companion object {
+        private const val ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN"
+        private const val ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS"
+        private const val ALLOWED_ORIGIN = "*"
+        private const val MAX_AGE = "3600"
+    }
+
     @Bean
-    fun corsFilter(): CorsFilter {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        config.addAllowedOrigin("*") // Allow localhost with port 3000
-        config.addAllowedHeader("*")
-        config.addAllowedMethod("*")
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
+    fun corsFilter(): WebFilter {
+        return WebFilter { ctx: ServerWebExchange, chain: WebFilterChain ->
+            val request: ServerHttpRequest = ctx.request
+            if (CorsUtils.isCorsRequest(request)) {
+                val response: ServerHttpResponse = ctx.response
+                val headers: HttpHeaders = response.headers
+                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN)
+                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS)
+                headers.add("Access-Control-Max-Age", MAX_AGE)
+                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS)
+                if (request.method == HttpMethod.OPTIONS) {
+                    response.statusCode = HttpStatus.OK
+                    return@WebFilter Mono.empty<Void>()
+                }
+            }
+            chain.filter(ctx)
+        }
     }
 }
