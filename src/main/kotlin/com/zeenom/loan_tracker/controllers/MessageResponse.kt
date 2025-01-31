6e09ff306d6e237be29e91b79928ebd545daa1e7
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-01 02:19:23 +0800

    moved to monorepo style

--- src/main/kotlin/com/zeenom/loan_tracker/controllers/MessageResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.zeenom.loan_tracker.controllers
-
-data class MessageResponse(val message: String)
--- src/main/kotlin/com/zeenom/loan_tracker/controllers/Paginated.kt
+++
@@ -1,6 +0,0 @@
-package com.zeenom.loan_tracker.controllers
-
-data class Paginated<T> (
-    val data: T,
-    val next: String?
-)
