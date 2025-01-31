c82f54a4f02e9d0b9c87e4371289f333f69b3986
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-01 04:28:44 +0800

    added firebase auth baseline

--- loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/controllers/AuthController.kt
+++ loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/controllers/AuthController.kt
@@ -1,34 +1,30 @@
 package com.zeenom.loan_tracker.controllers
 
 import com.zeenom.loan_tracker.services.FirebaseService
+import com.zeenom.loan_tracker.services.JWTToken
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
-import java.util.*
 
 @RestController
 @RequestMapping("/api/auth")
 class AuthController(val firebaseService: FirebaseService) {
 
-    private val secretKey = "your-secret-key" // Store this securely
-
     @PostMapping("/login")
-    fun login(@RequestHeader("Authorization") authHeader: String?): Mono<ResponseEntity<Map<String, String>>> {
+    fun login(@RequestHeader("Authorization") authHeader: String?): Mono<ResponseEntity<JWTToken>> {
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return Mono.just(ResponseEntity.badRequest().body(mapOf("error" to "Invalid token")))
+            return Mono.error(IllegalArgumentException("Invalid Authorization header"))
         }
-
         val firebaseIdToken = authHeader.substring(7)
         return firebaseService.generateJwtUsingIdToken(firebaseIdToken)
             .map { decodedToken ->
-                val customJwt = generateJwt(decodedToken)
-                ResponseEntity.ok(mapOf("token" to customJwt))
+                ResponseEntity.ok(decodedToken)
             }
             .onErrorResume {
-                Mono.just(ResponseEntity.badRequest().body(mapOf("error" to "Invalid Firebase token")))
+                Mono.error(IllegalArgumentException("Invalid Firebase ID token"))
             }
     }
 }
