f66d15fe880292235512ed687c7eb08be1ff0c11
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-03 00:42:48 +0800

    fixed login endpoint

--- loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/controllers/AuthController.kt
+++ loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/controllers/AuthController.kt
@@ -5,7 +5,7 @@ import com.zeenom.loan_tracker.services.AuthService
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,13 +14,13 @@ class AuthController(val authService: AuthService) {
     val logger = LoggerFactory.getLogger(AuthController::class.java)
 
     @PostMapping("/login")
-    suspend fun login(@RequestHeader("Authorization") authHeader: String?): ResponseEntity<JWTTokenResponse> {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+    suspend fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<JWTTokenResponse> {
+        if (!loginRequest.idToken.startsWith("Bearer ")) {
             throw IllegalArgumentException("Invalid Authorization header")
         }
-        val firebaseIdToken = authHeader.substring(7)
+        val idToken = loginRequest.idToken.substring(7)
         return try {
-            val decodedToken = authService.generateJwtUsingIdToken(firebaseIdToken)
+            val decodedToken = authService.generateJwtUsingIdToken(idToken)
             ResponseEntity.ok(JWTTokenResponse(token = decodedToken))
         } catch (e: Exception) {
             logger.error("Error generating JWT token", e)
@@ -29,3 +29,4 @@ class AuthController(val authService: AuthService) {
     }
 }
 
+data class LoginRequest(val idToken: String)
