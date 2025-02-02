83954dbc0ec8c81f2b50f20c5caea717bc89d492
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-03 01:05:47 +0800

    swagger updated

--- loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/configurations/SwaggerConfig.kt
+++ loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/configurations/SwaggerConfig.kt
@@ -1,8 +1,11 @@
 package com.zeenom.loan_tracker.configurations
 
 import com.zeenom.loan_tracker.properties.SelfProperties
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,6 +20,16 @@ class SwaggerConfig {
                 .version("1.0.0")
                 .description("API documentation for the Loan Tracker application")
         )
+            .components(
+                Components().addSecuritySchemes(
+                    "bearerAuth",
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+            .addSecurityItem(SecurityRequirement().addList("bearerAuth"))
             .servers(
                 listOf(
                     Server().url(selfProperties.apiUrl).description("Production server")
