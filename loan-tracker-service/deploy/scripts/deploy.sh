2103ac7ae7a34ebbd0340512680c48a7eca42e0d
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-08 06:12:19 +0800

    fixed network

--- loan-tracker-service/deploy/scripts/deploy.sh
+++ loan-tracker-service/deploy/scripts/deploy.sh
@@ -4,5 +4,5 @@ cd /home/ec2-user/app
 sudo docker pull zeeshan60/loan-tracker-service:latest
 sudo docker stop loantracker || true
 sudo docker container prune -f || true
-sudo docker run -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=dev --name loantracker zeeshan60/loan-tracker-service:latest
+sudo docker run --network host -d -p 8080:8080 -e SPRING_PROFILES_ACTIVE=dev --name loantracker zeeshan60/loan-tracker-service:latest
 sudo docker image prune -f || true
