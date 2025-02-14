f292a4cddc5810055d4de4cf12054f5dc2ee5ee0
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-14 14:05:42 +0800

    fixed invalid login fail message

--- loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/security/AuthController.kt
+++ loan-tracker-service/src/main/kotlin/com/zeenom/loan_tracker/security/AuthController.kt
@@ -1,8 +1,6 @@
 package com.zeenom.loan_tracker.security
 
-import com.google.firebase.auth.FirebaseAuthException
 import com.zeenom.loan_tracker.common.exceptions.UnauthorizedException
-import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -11,21 +9,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AuthController(val authService: AuthService) {
 
-    val logger = LoggerFactory.getLogger(AuthController::class.java)
-
     @PostMapping("/login")
     suspend fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<JWTTokenResponse> {
         if (!loginRequest.idToken.startsWith("Bearer ")) {
-            throw UnauthorizedException("Invalid Authorization header")
+            throw UnauthorizedException("Invalid id token. Expecting a Bearer token")
         }
         val idToken = loginRequest.idToken.substring(7)
-        return try {
-            val decodedToken = authService.generateJwtUsingIdToken(idToken)
-            ResponseEntity.ok(JWTTokenResponse(token = decodedToken))
-        } catch (e: FirebaseAuthException) {
-            logger.error("Error generating JWT token", e)
-            throw UnauthorizedException("Invalid Firebase ID token", e)
-        }
+        val decodedToken = authService.generateJwtUsingIdToken(idToken)
+        return ResponseEntity.ok(JWTTokenResponse(token = decodedToken))
     }
 }
 
