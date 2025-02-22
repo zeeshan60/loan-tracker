d810b96f6193a7d0241dff7df30f6a7692ec8af4
zeeshan <zeeshan.tufail@aboitiz.com>
2025-02-23 04:54:29 +0800

    Refactoring some more

--- loan-tracker-service/src/test/kotlin/com/zeenom/loan_tracker/controllers/TransactionsControllerIntegrationTest.kt
+++ loan-tracker-service/src/test/kotlin/com/zeenom/loan_tracker/controllers/TransactionsControllerIntegrationTest.kt
@@ -17,12 +17,15 @@ import java.util.UUID
 
 /**
  * Minimal Test case script
- * 1. Login as a new user during setup
- * 2. Add a friend
+ * 1. Setup. Login as a new user
+ * 2. Setup. Add a friend
  * 3. Add a transaction
  * 4. Get all transactions
  * 5. Create friend as a user
  * 6. Get all transactions by friend user
+ * 7. Update transaction
+ * 8. Get all transactions by main user
+ * 9. Get all transactions by friend user
  */
 class TransactionsControllerIntegrationTest_MinimalScript(@LocalServerPort private val port: Int) :
     BaseIntegration(port) {
