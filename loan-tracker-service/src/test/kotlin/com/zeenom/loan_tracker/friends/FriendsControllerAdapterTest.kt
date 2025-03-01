4282b172999cd93a013eb1c722a8c6b804e37f4c
zeeshan <zeeshan.tufail@aboitiz.com>
2025-03-01 20:03:09 +0800

    added currency conversion

--- loan-tracker-service/src/test/kotlin/com/zeenom/loan_tracker/friends/FriendsControllerAdapterTest.kt
+++ loan-tracker-service/src/test/kotlin/com/zeenom/loan_tracker/friends/FriendsControllerAdapterTest.kt
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test
 import java.util.*
 
 class FriendsControllerAdapterTest {
+
     @Test
-    fun `given balances takes first balance as main balance when main currency is not available`() {
+    fun `given balances takes main currency balance as main balance when main currency is available`() {
         // Arrange
         val friendDto = FriendDto(
             friendId = UUID.randomUUID(),
@@ -15,9 +16,13 @@ class FriendsControllerAdapterTest {
             phoneNumber = null,
             photoUrl = null,
             name = "John",
-            mainCurrency = null,
+            mainCurrency = Currency.getInstance("USD"),
             balances = AllTimeBalanceDto(
-                null, listOf(
+                AmountDto(
+                    amount = 200.0.toBigDecimal(),
+                    currency = Currency.getInstance("USD"),
+                    isOwed = false
+                ), listOf(
                     AmountDto(
                         amount = 100.0.toBigDecimal(),
                         currency = Currency.getInstance("SGD"),
@@ -45,56 +50,10 @@ class FriendsControllerAdapterTest {
         ).data.friends[0]
 
         // Assert
-        assertThat(result.mainBalance?.amount).isEqualTo(100.0.toBigDecimal())
-        assertThat(result.mainBalance?.currency).isEqualTo("SGD")
-        assertThat(result.mainBalance?.isOwed).isTrue()
-        assertThat(result.otherBalances.size).isEqualTo(1)
-        assertThat(result.otherBalances[0].amount).isEqualTo(200.0.toBigDecimal())
-        assertThat(result.otherBalances[0].currency).isEqualTo("USD")
-        assertThat(result.otherBalances[0].isOwed).isFalse()
-    }
-
-    @Test
-    fun `given balances takes main currency balance as main balance when main currency is available`() {
-        // Arrange
-        val friendDto = FriendDto(
-            friendId = UUID.randomUUID(),
-            email = null,
-            phoneNumber = null,
-            photoUrl = null,
-            name = "John",
-            mainCurrency = Currency.getInstance("USD"),
-            balances = AllTimeBalanceDto(null, listOf(
-                AmountDto(
-                    amount = 100.0.toBigDecimal(),
-                    currency = Currency.getInstance("SGD"),
-                    isOwed = true
-                ),
-                AmountDto(
-                    amount = 200.0.toBigDecimal(),
-                    currency = Currency.getInstance("USD"),
-                    isOwed = false
-                )
-            ))
-        )
-
-        // Act
-        val adapter = FriendsControllerAdapter()
-        val result = adapter.fromDtoToPaginatedResponse(
-            FriendsWithAllTimeBalancesDto(
-                listOf(friendDto),
-                balance = AllTimeBalanceDto(
-                    main = null,
-                    other = emptyList()
-                )
-            )
-        ).data.friends[0]
-
-        // Assert
         assertThat(result.mainBalance?.amount).isEqualTo(200.0.toBigDecimal())
         assertThat(result.mainBalance?.currency).isEqualTo("USD")
         assertThat(result.mainBalance?.isOwed).isFalse()
-        assertThat(result.otherBalances.size).isEqualTo(1)
+        assertThat(result.otherBalances.size).isEqualTo(2)
         assertThat(result.otherBalances[0].amount).isEqualTo(100.0.toBigDecimal())
         assertThat(result.otherBalances[0].currency).isEqualTo("SGD")
         assertThat(result.otherBalances[0].isOwed).isTrue()
