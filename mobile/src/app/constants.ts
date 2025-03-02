39b89d9b077ce8993a9b950b65a40c6edc90cccd
noman.tufail <noman.tufail@edetek.com>
2025-03-02 12:57:52 +0500

    Thai baht fixed

--- mobile/src/app/constants.ts
+++ mobile/src/app/constants.ts
@@ -3,4 +3,4 @@ import { environment } from '../environments/environment';
 export const DEFAULT_TOAST_DURATION = 2000;
 export const PRIVATE_API = `${environment.apiBaseUrl}/api/v1`;
 export const PUBLIC_API = environment.apiBaseUrl;
-export const CURRENCIES = [ 'PKR', 'USD', 'SGD', 'BAHT'];
+export const CURRENCIES = [ 'PKR', 'USD', 'SGD', 'THB'];
