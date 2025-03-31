8531efe7635451f0c3b0b91a5284cfa07a8fb4d8
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-31 19:06:31 +0800

    linked loantracker with native ios google signin

--- mobile/src/app/login/login.component.ts
+++ mobile/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { GoogleAuthProvider } from "firebase/auth";
-import { Auth, signInWithPopup } from '@angular/fire/auth';
-import { Router } from '@angular/router';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
+import {GoogleAuthProvider} from "firebase/auth";
+import {Auth, signInWithPopup} from '@angular/fire/auth';
+import {Router} from '@angular/router';
 import {
   IonButton,
   IonContent,
@@ -10,9 +10,27 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
-import { ToastController } from '@ionic/angular/standalone';
-import { UserCredential } from '@firebase/auth';
-import { AuthStore } from './auth.store';
+import {ToastController} from '@ionic/angular/standalone';
+import {UserCredential} from '@firebase/auth';
+import {AuthStore} from './auth.store';
+import {LoginPlugin} from "zeenom";
+
+const inputValue = JSON.stringify({
+  CLIENT_ID: "336545645239-ppcpb0k5hc8303p9ek0793f8lkbbqbku.apps.googleusercontent.com",
+  REVERSED_CLIENT_ID: "com.googleusercontent.apps.336545645239-ppcpb0k5hc8303p9ek0793f8lkbbqbku",
+  API_KEY: "AIzaSyB1qt9hOwlzyBWGIe-grrg0Vgp53tcwoLE",
+  GCM_SENDER_ID: "336545645239",
+  PLIST_VERSION: "1",
+  BUNDLE_ID: "com.zeenomlabs.loantracker",
+  PROJECT_ID: "loan-tracker-9b25d",
+  STORAGE_BUCKET: "loan-tracker-9b25d.firebasestorage.app",
+  IS_ADS_ENABLED: false,
+  IS_ANALYTICS_ENABLED: false,
+  IS_APPINVITE_ENABLED: true,
+  IS_GCM_ENABLED: true,
+  IS_SIGNIN_ENABLED: true,
+  GOOGLE_APP_ID: "1:336545645239:ios:90e69a58265af386220332"
+});
 
 @Component({
   selector: 'app-login',
@@ -28,13 +46,18 @@ export class LoginComponent {
   private authStore = inject(AuthStore);
   private toastController = inject(ToastController);
 
-  constructor() { }
+  constructor() {
+  }
 
   loginWithGoogle() {
     this.authStore.loginWithGoogle();
   }
 
   nativeLogin() {
-    // todo: trigger native plugin here...
+
+    LoginPlugin.echo({value: inputValue}).then(result => {
+        console.log("js: " + JSON.stringify(result));
+      }
+    );
   }
 }
