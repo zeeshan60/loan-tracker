7fbd9070e41abec1e4180f9e1092fa1bdfda1528
nomantufail <nomantufail100@gmail.com>
2025-04-18 18:47:32 +0500

    api failure retry implemented.

--- mobile/src/app/auth.interceptor.ts
+++ mobile/src/app/auth.interceptor.ts
@@ -1,6 +1,6 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, of, switchMap } from 'rxjs';
+import { catchError, Observable, of, retry, switchMap, throwError, timer } from 'rxjs';
 import { AuthStore } from './login/auth.store';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { AuthStore } from './login/auth.store';
 export class AuthInterceptor implements HttpInterceptor {
   readonly authStore = inject(AuthStore);
   intercept(req: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
-    return req.url.includes('/api/v1') ?
+    const reqHandler = req.url.includes('/api/v1') ?
       of(this.authStore.apiKey())
         .pipe(
           switchMap(authToken => handler.handle(req.clone({
@@ -17,5 +17,29 @@ export class AuthInterceptor implements HttpInterceptor {
           })))
         ) :
       handler.handle(req);
+
+    /**
+     * retry the api call which failed because of some unknown server failure
+     */
+    return reqHandler
+      .pipe(
+        retry({
+          count: 2,
+          delay: (error) => {
+            if (
+              error instanceof HttpErrorResponse &&
+              error.status >= 500 &&
+              error.status < 600
+            ) {
+              return timer(2000); // Delay before retry
+            }
+            // Don't retry for non-5xx errors
+            throw error;
+          }
+        }),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(() => error);
+        })
+    );
   }
 }
