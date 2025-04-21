b10801b74f85dcb45539416cedb874bdc669f457
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-04-21 21:20:07 +0800

    Overwritten prod config on dev for now

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -8,5 +8,5 @@ export const environment = {
     messagingSenderId: "336545645239",
     appId: "1:336545645239:web:42d70da2cdcc91d1220332"
   },
-  apiBaseUrl: "https://loanapi.codewithzeeshan.com",
+  apiBaseUrl: "https://loanapi-prod.codewithzeeshan.com",
 };
