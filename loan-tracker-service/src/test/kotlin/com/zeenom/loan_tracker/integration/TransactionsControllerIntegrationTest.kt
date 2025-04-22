1d44df8a222d89741cd70e96fc0fdbf0673f1d45
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-04-22 21:57:01 +0800

    checking activity logs bug

--- loan-tracker-service/src/test/kotlin/com/zeenom/loan_tracker/integration/TransactionsControllerIntegrationTest.kt
+++ loan-tracker-service/src/test/kotlin/com/zeenom/loan_tracker/integration/TransactionsControllerIntegrationTest.kt
@@ -1,8 +1,8 @@
 package com.zeenom.loan_tracker.integration
-
 import com.fasterxml.jackson.core.type.TypeReference
 import com.zeenom.loan_tracker.common.Paginated
 import com.zeenom.loan_tracker.friends.*
+import com.zeenom.loan_tracker.prettyAndPrint
 import com.zeenom.loan_tracker.transactions.*
 import com.zeenom.loan_tracker.users.UserDto
 import com.zeenom.loan_tracker.users.UserEventRepository
@@ -15,6 +15,98 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.Instant
 import java.util.*
+
+class DeleteFriendActivityLogsBugIntegrationTest: BaseIntegration() {
+    @Autowired
+    private lateinit var friendEventRepository: FriendEventRepository
+
+    @Autowired
+    private lateinit var userEventRepository: UserEventRepository
+
+    @Autowired
+    private lateinit var transactionEventRepository: TransactionEventRepository
+
+    private lateinit var zeeToken: String
+    private lateinit var johnToken: String
+    private lateinit var jasonToken: String
+    private var zeeDto = UserDto(
+        uid = "123",
+        email = "zee@gmail.com",
+        phoneNumber = "+923001234567",
+        displayName = "Zeeshan Tufail",
+        photoUrl = "https://lh3.googleusercontent.com/a/A9GpZGSDOI3TbzQEM8vblTl2",
+        currency = null,
+        emailVerified = true
+    )
+    private var jasonDto = UserDto(
+        uid = "125",
+        email = "jason@gmail.com",
+        phoneNumber = "+923001234569",
+        displayName = "Jason Doe",
+        photoUrl = "https://lh3.googleusercontent.com/a/A9GpZGSDOI3TbzQEM8vblTl4",
+        currency = null,
+        emailVerified = true
+    )
+    private lateinit var johnFriendId: UUID
+
+    private lateinit var zeeFriendId: UUID
+
+    @BeforeAll
+    fun setupBeforeAll(): Unit = runBlocking {
+        userEventRepository.deleteAll()
+        friendEventRepository.deleteAll()
+        transactionEventRepository.deleteAll()
+        zeeToken = loginUser(
+            userDto = zeeDto
+        ).token
+        addFriend(zeeToken, "john")
+        johnFriendId = queryFriend(
+            token = zeeToken
+        ).data.friends.first().friendId
+    }
+
+    @Order(1)
+    @Test
+    fun `given transaction and friend when delete friend fetches activity logs successfully`() {
+
+        webTestClient.post()
+            .uri("/api/v1/transactions/add")
+            .header("Authorization", "Bearer $zeeToken")
+            .bodyValue(
+                TransactionCreateRequest(
+                    amount = 100.0.toBigDecimal(),
+                    currency = "USD",
+                    type = SplitType.YouPaidSplitEqually,
+                    recipientId = johnFriendId,
+                    description = "Sample transaction",
+                    transactionDate = Instant.parse("2025-02-26T00:00:00Z")
+                )
+            )
+            .exchange()
+            .expectStatus().isOk
+            .expectBody().jsonPath("$.description").isEqualTo("Sample transaction")
+
+        webTestClient.delete()
+            .uri("/api/v1/friends/$johnFriendId")
+            .header("Authorization", "Bearer $zeeToken")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(String::class.java)
+            .returnResult().responseBody!!.also { it.prettyAndPrint(objectMapper) }
+
+        webTestClient.get()
+            .uri("/api/v1/transactions/activityLogs")
+            .header("Authorization", "Bearer $zeeToken")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(String::class.java)
+            .returnResult().responseBody!!.let {
+                objectMapper.readValue(
+                    it,
+                    object : TypeReference<Paginated<List<ActivityLogResponse>>>() {})
+            }
+    }
+}
 
 class TransactionsControllerIntegrationTest:
     BaseIntegration() {
