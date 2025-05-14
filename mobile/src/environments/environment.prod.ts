e83741af9ba2bb4021d6f7490c797b912e78c7cd
nomantufail <nomantufail100@gmail.com>
2025-05-14 12:56:34 +0500

    minor changes

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,27 +1,28 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAXabT3rtcr7bo55JZM1yoE2VOnhsIVv0Y",
-    authDomain: "login-plugin-cc824.firebaseapp.com",
-    projectId: "login-plugin-cc824",
-    storageBucket: "login-plugin-cc824.firebasestorage.app",
-    messagingSenderId: "394556941374",
-    appId: "1:394556941374:web:996e09945f6ca26b129d6f"
+    apiKey: "AIzaSyAo6WpRVyGz1L3bNZ6yVwI1BOgO0Ol5TzQ",
+    authDomain: "money-rabbit-6024.firebaseapp.com",
+    projectId: "money-rabbit-6024",
+    storageBucket: "money-rabbit-6024.firebasestorage.app",
+    messagingSenderId: "583697131660",
+    appId: "1:583697131660:web:f5dad90c99aa4538c6b3a9",
+    measurementId: "G-VE7V295V30"
   },
   firebaseAndroid: {
-    clientId: "394556941374-7t8udgrmmp21og7dhi7vqtqp920dop4g.apps.googleusercontent.com",
-    apiKey: "AIzaSyAXabT3rtcr7bo55JZM1yoE2VOnhsIVv0Y",
-    projectId: "login-plugin-cc824",
-    appId: "1:394556941374:android:1ff959a91a2cc95c129d6f"
+    clientId: "583697131660-fkk1tmi764guv1etl6s9605mhsjn22v0.apps.googleusercontent.com",
+    apiKey: "AIzaSyC1JvcM3SBmPWL0vSerB1Xl90vzqyPWPPw",
+    projectId: "money-rabbit-6024",
+    appId: "1:583697131660:android:d6e6142373dfd75cc6b3a9"
   },
   firebaseIOS: {
-    clientId: "394556941374-7t8udgrmmp21og7dhi7vqtqp920dop4g.apps.googleusercontent.com",
-    apiKey: "AIzaSyAXabT3rtcr7bo55JZM1yoE2VOnhsIVv0Y",
-    messagingSenderId: "394556941374",
-    bundleId: "com.zeenomlabs.loantracker",
-    projectId: "login-plugin-cc824",
-    storageBucket: "login-plugin-cc824.firebasestorage.app",
-    appId: "1:336545645239:ios:90e69a58265af386220332"
+    clientId: "583697131660-u684qmd3begs6mtv7vj7tcl8tvjdqg8s.apps.googleusercontent.com",
+    apiKey: "AIzaSyARIJ8iu3bYwZz58kYTlg2ihbG3PNQxM1w",
+    messagingSenderId: "583697131660",
+    bundleId: "com.zeenomlabs.moneyrabbit",
+    projectId: "money-rabbit-6024",
+    storageBucket: "money-rabbit-6024.firebasestorage.app",
+    appId: "1:583697131660:ios:c2be1fe0cf8b3d84c6b3a9"
   },
   apiBaseUrl: "https://loanapi-prod.codewithzeeshan.com",
 };
