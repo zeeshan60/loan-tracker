a4264cb256bc92287ba3198aed8f6b211ee5d4d5
nomantufail <nomantufail100@gmail.com>
2025-05-17 09:34:58 +0500

    git cache cleared

--- mobile/android/app/src/main/java/com/zeenomlabs/moneyrabbit/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.zeenomlabs.moneyrabbit;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- mobile/src/app/pipes/shorten-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortenNamePipe } from './shorten-name.pipe';
-
-describe('ShortenNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortenNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
