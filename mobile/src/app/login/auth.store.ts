670c2330f4fcc96632d8321026a1c44f238efc8d
nomantufail <nomantufail100@gmail.com>
2025-06-20 16:23:52 +0500

    loader after signing issue fixed

--- mobile/src/app/login/auth.store.ts
+++ mobile/src/app/login/auth.store.ts
@@ -110,12 +110,14 @@ export const AuthStore = signalStore(
     },
 
     async loginWithGoogle(): Promise<boolean | void> {
-
-      const loginPromise = this.getLoginPluginToken();
-
-      return loginPromise
-        .then(async (token) => this.login(token!))
+      const loader = await loadingCtrl.create();
+      loader.present();
+      return this.getLoginPluginToken()
+        .then(async (token) => {
+          return this.login(token!);
+        })
         .then(() => {
+          loader.dismiss();
           if (!store.user()?.phoneNumber) {
             this.askForPhoneNumber();
           }
@@ -125,6 +127,9 @@ export const AuthStore = signalStore(
           await helperService.showToast('Unable to login at the moment', 2000, {
             color: 'danger'
           });
+        })
+        .finally(() => {
+          loader?.dismiss();
         });
     },
 
@@ -157,9 +162,7 @@ export const AuthStore = signalStore(
     },
 
     async login(idToken: string) {
-      const loader = await loadingCtrl.create();
       try {
-        loader.present();
         const url = `${PUBLIC_API}/login`
         const {token: apiKey} = await firstValueFrom(
           http.post<{ token: string }>(url, {
@@ -176,8 +179,6 @@ export const AuthStore = signalStore(
       } catch (e) {
         this.signOut();
         throw new Error('login failed.');
-      } finally {
-        loader.dismiss();
       }
     },
     async setApiKey() {
