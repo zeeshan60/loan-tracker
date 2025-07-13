c7068131410d39209480e09f2bf2577ffe985e7a
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-07-13 21:55:35 +0500

    fixing migration failure

--- loan-tracker-service/src/main/resources/db/migration/V14__add_friend_id.sql
+++ loan-tracker-service/src/main/resources/db/migration/V14__add_friend_id.sql
@@ -5,7 +5,7 @@ UPDATE friend_events
 SET friend_id = (select stream_id
                  from user_model um
                  where (um.email is not null and um.email = friend_events.friend_email)
-                    or (um.phone_number is not null and um.phone_number = friend_events.friend_phone_number))
+                    or (um.phone_number is not null and um.phone_number = friend_events.friend_phone_number) limit 1)
 WHERE friend_id IS NULL and friend_events.event_type = 'FRIEND_CREATED';
 
 ALTER TABLE friend_model
